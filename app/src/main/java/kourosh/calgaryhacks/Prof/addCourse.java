1694bcfd5f97c9a9a5d2d1893e7d4e385ba7450c
nasir <nasirosman1@hotmail.com>
2017-02-22 11:08:08 -0700

    Merge branch 'Prof/StudentUIPlus' into HEAD
    # Conflicts:
    #	.idea/modules.xml
    #	.idea/vcs.xml
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/kourosh/calgaryhacks/MainActivity.java
    #	app/src/main/res/layout/activity_main.xml

---
+++ app/src/main/java/kourosh/calgaryhacks/Prof/addCourse.java
@@ -0,0 +1,16 @@
+package kourosh.calgaryhacks.Prof;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import kourosh.calgaryhacks.R;
+
+public class addCourse extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_course);
+
+    }
+}
---
+++ app/src/main/java/login/RegisterActivity.java
@@ -0,0 +1,34 @@
+package login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import kourosh.calgaryhacks.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    Spinner dropdown;
+
+    public String email;
+    public String password;
+    public String firstName;
+    public String lastName;
+    public String spinnerChoice;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        populateSpinner();
+    }
+
+    private void populateSpinner() {
+        dropdown = (Spinner)findViewById(R.id.register_spinner);
+        String[] items = new String[]{"Student", "Professor"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        dropdown.setAdapter(adapter);
+    }
+}
