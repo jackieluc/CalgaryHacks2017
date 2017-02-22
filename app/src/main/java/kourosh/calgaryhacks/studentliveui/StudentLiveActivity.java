ec19e3b8145a59d923475b6a15d6f48fdd084831
Jackie Luc <jackie_luc@hotmail.com>
2017-02-21 22:20:43 -0700

    fix login centered & added new student live UI

---
+++ app/src/main/java/kourosh/calgaryhacks/studentliveui/StudentLiveActivity.java
@@ -0,0 +1,14 @@
+package kourosh.calgaryhacks.studentliveui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import kourosh.calgaryhacks.R;
+
+public class StudentLiveActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_live);
+    }
+}
