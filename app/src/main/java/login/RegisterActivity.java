384f5d1b7cdb59e2efc6914d201f17380261441b
Ahmed Elbannan <ahmed3lbannan@gmail.com>
2017-02-22 13:25:22 -0700

    Registration now work

--- app/src/main/java/login/RegisterActivity.java
+++ app/src/main/java/login/RegisterActivity.java
@@ -1,34 +1,64 @@
 package login;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import kourosh.calgaryhacks.Authenticator;
+import kourosh.calgaryhacks.AuthenticatorActivity;
 import kourosh.calgaryhacks.R;
 
-public class RegisterActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity implements AuthenticatorActivity
+{
 
-    Spinner dropdown;
-
-    public String email;
-    public String password;
-    public String firstName;
-    public String lastName;
-    public String spinnerChoice;
+    // UI references.
+    private AutoCompleteTextView mEmailView;
+    private EditText mPasswordView;
+    private Button mRegisterButton;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        populateSpinner();
+        // Set up the login form.
+        mEmailView = (AutoCompleteTextView) findViewById(R.id.register_email);
+        mPasswordView = (EditText) findViewById(R.id.register_password);
+        mRegisterButton = (Button) findViewById(R.id.registerButton);
+
+        Authenticator.current.setActivity(this);
+
+        mRegisterButton.setOnClickListener((View v) -> Authenticator.current.createNewUser(getEmail(), getPassword()));
     }
 
-    private void populateSpinner() {
-        dropdown = (Spinner)findViewById(R.id.register_spinner);
-        String[] items = new String[]{"Student", "Professor"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, items);
-        dropdown.setAdapter(adapter);
+    private String getEmail()
+    {
+        return mEmailView.getText().toString().trim();
+    }
+
+    private String getPassword()
+    {
+        return mPasswordView.getText().toString().trim();
+    }
+
+    public void setEmailError(String error)
+    {
+        mEmailView.setError(error);
+    }
+
+    public void setPasswordError(String error)
+    {
+        mPasswordView.setError(error);
+    }
+
+    public void onSuccess()
+    {
+
     }
 }
